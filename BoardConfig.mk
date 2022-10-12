4fb82e928a595cc86e27908bbdd80235b55cd8ea
sthomsonpl <tomekciupa07.02@wp.pl>
2022-10-12 23:38:46 +0200

    switch to hardware/oplus

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_OTA_ASSERT_DEVICE := OnePlus6T,oneplus6t,fajita
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # FOD
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oneplus:libudfps_extension.oneplus
+TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
